f6d59340f9bdba2dc352be207c01ce19ced79a0a
OguzhanGrey <oguz271402@gmail.com>
2025-02-24 18:24:44 +0300

    a

--- js/gallery.js
+++ js/gallery.js
@@ -105,3 +105,16 @@ gallery.addEventListener("click", (event) => {
     modal.close();
   });
 });
+
+const form = document.querySelector(".feedback-form");
+const localStorageKey = "goit-example-message";
+
+form.addEventListener("input", (evt) => {
+  localStorage.setItem(localStorageKey, evt.target.value);
+});
+
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+	console.log(evt.target.elements.message.value);
+  form.reset();
+});
